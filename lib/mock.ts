6bea35f9b3be62746bc1696fbe82212ca7b42961
Batt866 <25LP6030@nest.edu.mn>
2026-02-06 09:24:01 +0800

    g

--- lib/mock.ts
+++ lib/mock.ts
@@ -2,8 +2,8 @@ export const menuItems = [
   { label: "Home", ariaLabel: "Go to home page", link: "#home" },
   { label: "About", ariaLabel: "Learn about us", link: "#about" },
   { label: "Services", ariaLabel: "View our services", link: "#services" },
-  { label: "Contact", ariaLabel: "Get in touch", link: "#contact" },
   { label: "Product", ariaLabel: "View our products", link: "#product" },
+  { label: "Contact", ariaLabel: "Get in touch", link: "#contact" },
 ];
 
 export const socialItems = [
