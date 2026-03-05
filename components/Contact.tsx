e55ae7a33c528df7d830e42c61c76a8a58f2a2d4
ankhush ah <anhfurev@gmail.com>
2026-03-05 10:01:26 +0800

    a

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -89,7 +89,7 @@ export function Contact({ isMenuOpen }: { isMenuOpen: boolean }) {
                 }}
                 className=" flex flex-col justify-center items-center"
               >
-                <div className="lg:max-w-4xl max-w-3xl m-auto justify-center items-center">
+                <div className="lg:max-w-5xl max-w-4xl m-auto justify-center items-center">
                   <motion.div
                     initial={{ opacity: 0, y: 40 }}
                     animate={isInView ? { opacity: 1, y: 0 } : {}}
